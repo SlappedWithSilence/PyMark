ad0f9e6dde06a9e50244b846a0d80e187f51d1d5
SlappedWithSilence <asigal55+02@gmail.com>
2024-10-14 12:49:34 -0700

    Added skeleton for watermark logic

---
+++ src/helpers/image.py
@@ -0,0 +1,9 @@
+def apply_watermark(image_path: str, watermark_path: str, corner: str, padding: float) -> None:
+    """
+
+    :param image_path:
+    :param watermark_path:
+    :param corner:
+    :param padding:
+    :return:
+    """
